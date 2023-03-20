97862418457930c614d5df27f356e07a2b150a4d
eran <eranavru100@gmail.com>
2023-03-20 22:03:23 +0200

    Update deleteDishFromCart.ts

--- src/utils/UserUtils/deleteDishFromCart.ts
+++ src/utils/UserUtils/deleteDishFromCart.ts
@@ -3,12 +3,15 @@ import axios from "axios";
 
 
 export const deleteFromCartDB = async (dishId: number) => {
-    try {
+    const userDataString = (sessionStorage.getItem('user-logged-in'));
+    const userDataObj =userDataString && JSON.parse(userDataString)
+        try {
       const deleteItem = await axios.delete(
         "https://eran-epicure-project-back.onrender.com/api/users/delete/cartDish",
         {
           data: {
             dishId: dishId,
+            user: userDataObj.email
           },
           headers: {
             Authorization: `Bearer ${sessionStorage.getItem("user-token")}`,
